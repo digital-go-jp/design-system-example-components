d728837da6147a33e5dd61a642a6cce4f8a5fe04
SHIMADA Takayuki <takayshim@digital.go.jp>
2024-10-29 15:26:57 +0900

    fix: fix LanguageSelector's bug of Safari

--- src/components/LanguageSelector/parts/Button.tsx
+++ src/components/LanguageSelector/parts/Button.tsx
@@ -9,7 +9,7 @@ export const LanguageSelectorButton = forwardRef<HTMLButtonElement, LanguageSele
     return (
       <button
         className={`
-          flex w-fit gap-1 items-center px-2 min-h-[calc(44/16*1rem)] border border-transparent text-oln-16N-100 text-solid-gray-800 rounded-lg
+          flex w-fit gap-1 items-center px-2 min-h-[calc(44/16*1rem)] text-oln-16N-100 text-solid-gray-800 rounded-lg
           hover:bg-solid-gray-50 hover:underline hover:underline-offset-[calc(3/16*1rem)]
           focus-visible:outline focus-visible:outline-4 focus-visible:outline-black focus-visible:outline-offset-[calc(2/16*1rem)] focus-visible:ring-[calc(2/16*1rem)] focus-visible:ring-yellow-300 focus-visible:bg-yellow-300
           ${className ?? ''}
