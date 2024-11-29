4ccfdd94c2ed02d0bdfd1b6609a20fcaa47d5964
keisuke.okafuji <keiokafuj@digital.go.jp>
2024-11-29 15:04:42 +0900

    feat: improve Ul and Ol components

--- src/components/Ol/Ol.stories.tsx
+++ src/components/Ol/Ol.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import React from 'react';
+import { Ul } from '../';
 import { Ol } from './Ol';
 
 const meta = {
@@ -11,10 +12,10 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Example: Story = {
+export const Spacing4: Story = {
   render: () => {
     return (
-      <Ol>
+      <Ol className='[&_:where(ol,ul)]:mt-1 [&_:where(ol,ul)]:-mb-1 [&_li]:py-1'>
         <li>サンプルサンプル</li>
         <li>サンプルサンプル</li>
         <li>サンプルサンプル</li>
@@ -24,9 +25,125 @@ export const Example: Story = {
             <li>サンプルサンプル</li>
             <li>サンプルサンプル</li>
             <li>サンプルサンプル</li>
+            <li>
+              サンプルサンプル
+              <Ol>
+                <li>サンプルサンプル</li>
+                <li>サンプルサンプル</li>
+                <li>サンプルサンプル</li>
+                <li>サンプルサンプル</li>
+              </Ol>
+            </li>
             <li>サンプルサンプル</li>
           </Ol>
         </li>
+        <li>サンプルサンプル</li>
+      </Ol>
+    );
+  },
+};
+
+export const Spacing8: Story = {
+  render: () => {
+    return (
+      <Ol className='[&_:where(ol,ul)]:mt-2 [&_:where(ol,ul)]:-mb-2 [&_li]:py-2'>
+        <li>サンプルサンプル</li>
+        <li>サンプルサンプル</li>
+        <li>サンプルサンプル</li>
+        <li>
+          サンプルサンプル
+          <Ol>
+            <li>サンプルサンプル</li>
+            <li>サンプルサンプル</li>
+            <li>サンプルサンプル</li>
+            <li>
+              サンプルサンプル
+              <Ol>
+                <li>サンプルサンプル</li>
+                <li>サンプルサンプル</li>
+                <li>サンプルサンプル</li>
+                <li>サンプルサンプル</li>
+              </Ol>
+            </li>
+            <li>サンプルサンプル</li>
+          </Ol>
+        </li>
+        <li>サンプルサンプル</li>
+      </Ol>
+    );
+  },
+};
+
+export const Spacing12: Story = {
+  render: () => {
+    return (
+      <Ol className='[&_:where(ol,ul)]:mt-3 [&_:where(ol,ul)]:-mb-3 [&_li]:py-3'>
+        <li>サンプルサンプル</li>
+        <li>サンプルサンプル</li>
+        <li>サンプルサンプル</li>
+        <li>
+          サンプルサンプル
+          <Ol>
+            <li>サンプルサンプル</li>
+            <li>サンプルサンプル</li>
+            <li>サンプルサンプル</li>
+            <li>
+              サンプルサンプル
+              <Ol>
+                <li>サンプルサンプル</li>
+                <li>サンプルサンプル</li>
+                <li>サンプルサンプル</li>
+                <li>サンプルサンプル</li>
+              </Ol>
+            </li>
+            <li>サンプルサンプル</li>
+          </Ol>
+        </li>
+        <li>サンプルサンプル</li>
+      </Ol>
+    );
+  },
+};
+
+export const WithLowerLatin: Story = {
+  render: () => {
+    return (
+      <Ol className='[&_:where(ol,ul)]:mt-1 [&_:where(ol,ul)]:-mb-1 [&_li]:py-1'>
+        <li>サンプルサンプル</li>
+        <li>サンプルサンプル</li>
+        <li>サンプルサンプル</li>
+        <li>
+          サンプルサンプル
+          <Ol className='list-lower-latin'>
+            <li>サンプルサンプル</li>
+            <li>サンプルサンプル</li>
+            <li>サンプルサンプル</li>
+            <li>サンプルサンプル</li>
+          </Ol>
+        </li>
+        <li>サンプルサンプル</li>
+      </Ol>
+    );
+  },
+};
+
+export const WithUnorderedList: Story = {
+  render: () => {
+    return (
+      <Ol className='[&_:where(ol,ul)]:mt-1 [&_:where(ol,ul)]:-mb-1 [&_li]:py-1'>
+        <li>サンプルサンプル</li>
+        <li>サンプルサンプル</li>
+        <li>サンプルサンプル</li>
+        <li>
+          サンプルサンプル
+          <Ul>
+            <li>サンプルサンプル</li>
+            <li>サンプルサンプル</li>
+            <li>サンプルサンプル</li>
+            <li>サンプルサンプル</li>
+          </Ul>
+        </li>
+        <li>サンプルサンプル</li>
       </Ol>
     );
   },
