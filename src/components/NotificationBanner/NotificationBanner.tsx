c6c3f825e83d26f61b74099c5c95bcb94749c171
SHIMADA Takayuki <takayshim@digital.go.jp>
2025-07-29 21:44:54 +0900

    feat(notification-banner): update close button size and hover style

--- src/components/NotificationBanner/NotificationBanner.tsx
+++ src/components/NotificationBanner/NotificationBanner.tsx
@@ -16,14 +16,14 @@ export const NotificationBanner = (props: Props) => {
   return (
     <div
       className={`
-        grid grid-cols-[var(--icon-size)_1fr_minmax(0,auto)] border-current p-4 [--icon-size:calc(24/16*1rem)] gap-x-3 gap-y-4
+        grid grid-cols-[var(--icon-size)_1fr_minmax(0,auto)] border-current p-4 pt-2 [--icon-size:calc(24/16*1rem)] gap-x-3 gap-y-4
         desktop:gap-x-6 desktop:p-6 desktop:[--icon-size:calc(36/16*1rem)]
         ${bannerStyleClasses[bannerStyle]}
         ${bannerTypeClasses[type]}
         ${className ?? ''}
       `}
     >
-      <div className='mt-[calc(2/16*1rem)] desktop:mt-0'>
+      <div className='mt-1.5 self-start desktop:mt-0'>
         <NotificationBannerIcon className='h-auto max-w-full' type={type} />
       </div>
 
--- src/components/NotificationBanner/parts/Heading.tsx
+++ src/components/NotificationBanner/parts/Heading.tsx
@@ -11,7 +11,7 @@ export const NotificationBannerHeading = (props: NotificationBannerHeadingProps)
 
   return (
     <Tag
-      className={`text-std-17B-170 text-solid-gray-900 col-start-1 desktop:mt-0.5 desktop:text-std-20B-160 ${className ?? ''}`}
+      className={`mt-1 text-std-17B-170 text-solid-gray-900 col-start-1 desktop:mt-0.5 desktop:text-std-20B-160 ${className ?? ''}`}
       {...rest}
     >
       {children}
